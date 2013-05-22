ca37f7c8f7ca263718a8b6dc19c11c651549ad2d
Josh Williams <jwilliams@endpoint.com>
2013-05-22 15:51:02 -0400

    Remove community aws componenets to avoid merges

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,32 +1,9 @@
 #
-# Cookbook Name:: aws
+# Cookbook Name:: aws-encrypted
 # Recipe:: default
 #
-# Copyright 2008-2009, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 
-r = gem_package "right_aws" do
-  version node['aws']['right_aws_version']
-  action :nothing
-end
-
-r.run_action(:install)
-
-require 'rubygems'
-Gem.clear_paths
-require 'right_aws'
+include_recipe 'aws'
 
 # Manage AWS access keys as requested via node attributes (done immediately)
 aws_access_keys_entries = node['aws']['access_keys']
@@ -38,7 +15,7 @@ unless aws_access_keys_entries.nil?
       :set
     end
 
-    aws_access_keys name do
+    aws_keys_access_keys name do
       attributes.each do |k, v|
         self.send(k.to_sym, v)
       end
